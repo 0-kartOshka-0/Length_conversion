68f2faca451bd03c8987bc678a007518b49538d8
0-kartOshka-0 <darazajceva3675@mail.ru>
2024-08-17 15:49:04 +0300

    length conversion

---
+++ Перевод длины/Задание_1_3/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double m = 0;
+        private bool show = false;
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Back)
+                return;
+            if (e.KeyChar == ',')
+            {
+                var textbox = sender as TextBox;
+                if (textbox.Text.Contains(','))
+                    e.Handled = true;
+                return;
+            }
+            if (char.IsDigit(e.KeyChar) || e.KeyChar == 8)
+                return;
+            e.Handled = true;
+        }
+        private void метрыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStrip1.Enabled = true;
+            try
+            {
+                if (sender == метрыToolStripMenuItem)
+                {
+                    label3.Text = "метры";
+                    m = double.Parse(textBox1.Text);
+                }
+                    
+                else if (sender == вершокToolStripMenuItem)
+                {
+                    label3.Text = "вершки";
+                    m = double.Parse(textBox1.Text) / 44.45;
+                }
+                else if (sender == аршинToolStripMenuItem)
+                {
+                    label3.Text = "аршины";
+                    m = double.Parse(textBox1.Text) / 0.7112;
+                }  
+                else if (sender == саженьToolStripMenuItem)
+                {
+                    label3.Text = "сажени";
+                    m = double.Parse(textBox1.Text) / 1.905;
+                }
+                else if (sender == верстаToolStripMenuItem)
+                {
+                    label3.Text = "версты";
+                    m = double.Parse(textBox1.Text) / 0.007;
+                }
+                else
+                    label3.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("Введены некорректные данные.");
+                toolStrip1.Enabled = false;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double result = 0;
+                if (sender == toolStripButton1)
+                {
+                   label4.Text = "метры";
+                    result = m;
+                }
+                else if (sender == toolStripButton2)
+                {
+                   label4.Text = "вершки";
+                    result = m * 44.45;
+                }
+                else if (sender == toolStripButton3)
+                {
+                   label4.Text = "сажени";
+                    result = m * 1.905;
+                }
+                else if (sender == toolStripButton4)
+                {
+                   label4.Text = "аршины";
+                    result = m * 0.7112;
+                }
+                else if (sender == toolStripButton5)
+                {
+                   label4.Text = "версты";
+                    result = m * 0.007;
+                }
+                textBox2.Text = ("" + result);
+                //toolStrip1.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("Введены некорректные данные.");
+            }
+        }
+
+        private void текстКнопкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (show)
+            {
+                toolStrip1.Visible = true;
+                show = false;
+            }
+            else
+            {
+                toolStrip1.Visible = false;
+                show = true;
+            }
+        }
+        private void настройкаИнтерфейсаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Owner = this;
+            form2.ShowDialog();
+
+            this.Size = form2.FormSize;
+            this.WindowState = form2.Maximizade;
+            this.Location = form2.Point;
+            //this.Color = form2.Point;
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox1 = new AboutBox1();
+            aboutBox1.Owner = this;
+            aboutBox1.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Перевод длины/Задание_1_3/Form2.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Задание_1_3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(144, 150);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(20, 20);
+            this.label4.TabIndex = 21;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "по центру экрана",
+            "сверху по центру экрана",
+            "снизу по центру экрана",
+            "на весь экран"});
+            this.comboBox1.Location = new System.Drawing.Point(122, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 27);
+            this.comboBox1.TabIndex = 20;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(64, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 27);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Выбрать цвет формы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(148, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(148, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(13, 25);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 19);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Расположение:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(68, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 19);
+            this.label2.TabIndex = 15;
+            this.label2.Text = " Ширина:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(71, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 19);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Высота:";
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.SystemColors.GradientActiveCaption;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(331, 248);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Настройка интерфейса";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
